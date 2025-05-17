339dca73390e0c2bbca7087d5801c09120bf6bdf
RX90 <aleksandrnikonov020@gmail.com>
2025-05-17 05:02:54 +0500

    working on chat

---
+++ schema/000001_init.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE users;
+
+DROP TABLE chats;
+
+DROP TABLE chat_members;
+
+DROP TABLE messages;
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id            serial PRIMARY KEY,
+    login         varchar(255) NOT NULL UNIQUE,
+    password_hash varchar(255) NOT NULL,
+    name          varchar(255) NOT NULL,
+    role          varchar(50)  NOT NULL
+);
+
+CREATE TABLE chats (
+    id          serial PRIMARY KEY,
+    name        varchar(255) NOT NULL,
+    created_by  integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE chat_members (
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    chat_id integer NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, chat_id)
+);
+
+CREATE TABLE messages (
+    id         serial PRIMARY KEY,
+    chat_id    integer NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
+    sender_id  integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    content    text NOT NULL
+);
